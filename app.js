0ba10caa38a7081d2932c5295de8ded71d6b2769
Akhil Rangi <akhil.rangi@aimbeyond.com>
2016-10-21 15:52:57 +0530

    code for assignment1

---
+++ app.js
@@ -0,0 +1,24 @@
+(function () {
+'use strict';
+
+var app = angular.module('LunchCheck', []);
+
+app.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishes = "";
+  $scope.placeholder="list comma separated dishes you usually have for lunch";
+  $scope.msg = "";
+
+  $scope.check = function () {
+    var str = $scope.dishes.split(',');
+    var count = str.length;
+    
+    if (count >= 1 && count <= 3)
+    {$scope.msg = "Enjoy!";}
+    else if (count >= 4)
+    {$scope.msg = "Too much!";}
+  };
+}
+})();
